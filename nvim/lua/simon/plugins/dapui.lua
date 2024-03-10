7e1ac1b245d28cca397567fb614458a3bc294977
simon <simontran18@gmail.com>
2024-03-10 01:30:51 -0500

    Added javascript debugger

--- nvim/lua/simon/plugins/dapui.lua
+++ nvim/lua/simon/plugins/dapui.lua
@@ -6,3 +6,25 @@ require("mason-nvim-dap").setup()
 require("dap-python").setup("/Users/simontran/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
 
 dapui.setup()
+require("dap").adapters["pwa-node"] = {
+	type = "server",
+	host = "localhost",
+	port = "${port}",
+	executable = {
+		command = "node",
+		-- 💀 Make sure to update this path to point to your installation
+		args = {
+			"/Users/simontran/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+			"${port}",
+		},
+	},
+}
+require("dap").configurations.javascript = {
+	{
+		type = "pwa-node",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+	},
+}
