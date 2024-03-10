69fb516e5daf81a416c6c561e604c09edaadd11b
simon <simontran18@gmail.com>
2024-03-10 14:29:52 -0400

    configured llvm c debugging

--- nvim/lua/simon/plugins/dapui.lua
+++ nvim/lua/simon/plugins/dapui.lua
@@ -6,7 +6,9 @@ require("mason-nvim-dap").setup()
 require("dap-python").setup("/Users/simontran/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
 
 dapui.setup()
-require("dap").adapters["pwa-node"] = {
+local dap = require("dap")
+dap.set_log_level("DEBUG")
+dap.adapters["pwa-node"] = {
 	type = "server",
 	host = "localhost",
 	port = "${port}",
@@ -19,7 +21,7 @@ require("dap").adapters["pwa-node"] = {
 		},
 	},
 }
-require("dap").configurations.javascript = {
+dap.configurations.javascript = {
 	{
 		type = "pwa-node",
 		request = "launch",
@@ -28,3 +30,37 @@ require("dap").configurations.javascript = {
 		cwd = "${workspaceFolder}",
 	},
 }
+dap.adapters.lldb = {
+	type = "executable",
+	command = "/usr/local/cellar/llvm/17.0.6_1/bin/lldb-vscode",
+	name = "lldb",
+}
+
+dap.configurations.cpp = {
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+
+		-- 💀
+		-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+		--
+		--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+		--
+		-- Otherwise you might get the following error:
+		--
+		--    Error on launch: Failed to attach to the target process
+		--
+		-- But you should be aware of the implications:
+		-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+		-- runInTerminal = false,
+	},
+}
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
