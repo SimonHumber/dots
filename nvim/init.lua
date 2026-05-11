bf55fcb99f5d9e2cb3718cd10b43ef6849db6ab8
simon <simontran18@gmail.com>
2026-05-10 20:51:38 -0400

    add rust lsp, setup typescript treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,13 +12,20 @@ vim.pack.add({
   { src = "https://github.com/windwp/nvim-autopairs.git" },
   { src = "https://github.com/NvChad/nvim-colorizer.lua.git" },
   { src = "https://github.com/nvim-lualine/lualine.nvim.git" },
-  { src = "https://github.com/EdenEast/nightfox.nvim.git" },
   { src = "https://github.com/lewis6991/gitsigns.nvim.git" },
   { src = "https://github.com/kylechui/nvim-surround.git" },
   {
     src = 'https://github.com/Saghen/blink.cmp',
     version = vim.version.range('*')
   },
+  -- themes
+  { src = "https://github.com/EdenEast/nightfox.nvim.git" },
+  { src = "https://github.com/bluz71/vim-nightfly-colors.git" },
+  { src = "https://github.com/uloco/bluloco.nvim.git" },
+  { src = "https://github.com/rktjmp/lush.nvim.git" },
+  { src = "https://github.com/folke/tokyonight.nvim.git" },
+  { src = "https://github.com/Tsuzat/NeoSolarized.nvim.git" },
+  { src = "https://github.com/craftzdog/solarized-osaka.nvim.git" },
 })
 
 -- Force Neovim to recognize the plugins added above before requiring them
@@ -27,10 +34,11 @@ vim.cmd("packloadall")
 -- 2. Setup (Direct requires)
 
 -- Theme
-vim.cmd.colorscheme("nightfox")
+require("solarized-osaka").setup({ styles = { functions = { bold = true } }, transparent = false })
+vim.cmd.colorscheme("solarized-osaka")
 
 -- UI & Utilities
-require("lualine").setup({ options = { theme = "nightfox" } })
+require("lualine").setup({ options = { theme = "horizon" } })
 require("colorizer").setup()
 require("nvim-autopairs").setup()
 require("nvim-surround").setup()
@@ -38,16 +46,20 @@ require("gitsigns").setup()
 
 -- LSP
 require("mason").setup()
-vim.lsp.enable({ "lua_ls", "ty", "ruff", "vtsls", "jsonls", "gopls", "postgres_lsp" })
+vim.lsp.enable({ "lua_ls", "ty", "ruff", "vtsls", "jsonls", "gopls", "postgres_lsp", "rust_analyzer" })
+vim.lsp.config["rust_analyzer"] = { settings = { ["rust-analyzer"] = { diagnostics = { experimental = { enable = true } } } } }
 vim.lsp.config["postgres_lsp"] = {
   cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/postgres-language-server"), "lsp-proxy" },
   workspace_required = false,
   root_markers = { vim.fn.stdpath("config") .. "/postgres-language-server.jsonc" },
 }
+-- Error message next to code
+vim.diagnostic.config({ virtual_text = true })
 
 -- Autocomplete and treesitter
 require('blink.cmp').setup({ keymap = { preset = 'super-tab' }, })
+-- :set filetype? to check filetype
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'go', "python", "javascript", "typescript", "sql" },
+  pattern = { 'go', "python", "javascript", "typescript", "sql", "toml", "rust", "typescriptreact", "javascriptreact" },
   callback = function() vim.treesitter.start() end,
 })
